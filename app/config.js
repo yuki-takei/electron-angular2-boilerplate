6959a3ff72b9ef143378561173adcbedee5c78e4
Yuki Takei <yuki@weseek.co.jp>
2015-08-19 02:17:39 +0900

    upgrade jspm

--- app/config.js
+++ app/config.js
@@ -14,9 +14,6 @@ System.config({
     "app": {
       "defaultExtension": "ts",
       "meta": {
-        "*.ts": {
-          "loader": "ts"
-        },
         "*.css": {
           "loader": "css"
         }
@@ -29,16 +26,12 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.13",
     "es6-shim": "github:es-shims/es6-shim@0.33.0",
     "reflect-metadata": "npm:reflect-metadata@0.1.0",
-    "ts": "github:frankwallis/plugin-typescript@2.0.4",
-    "typescript": "npm:typescript@1.5.3",
-    "github:frankwallis/plugin-typescript@2.0.4": {
-      "typescript": "npm:typescript@1.5.3"
-    },
+    "typescript": "npm:typescript@1.6.0-dev.20150818",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.3.2"
+      "buffer": "npm:buffer@3.4.2"
     },
     "github:jspm/nodelibs-os@0.1.0": {
       "os-browserify": "npm:os-browserify@0.1.2"
@@ -67,7 +60,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:buffer@3.3.2": {
+    "npm:buffer@3.4.2": {
       "base64-js": "npm:base64-js@0.0.8",
       "ieee754": "npm:ieee754@1.1.6",
       "is-array": "npm:is-array@1.0.1"
@@ -90,7 +83,7 @@ System.config({
     "npm:rx@2.5.1": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:typescript@1.5.3": {
+    "npm:typescript@1.6.0-dev.20150818": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
